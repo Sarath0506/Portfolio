8728880af131bd275c2d892c9019a6a54b5203d5
Sarath Meduru <medurusarath0506@gmail.com>
2024-11-13 02:41:09 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,227 @@
+import eduquestImage from "../images/edu-login.png";
+import shoppingCartImage from "../images/project -1 bi.jpg";
+import powerPredictionImage from "../images/project -2.gif";
+
+export const Bio = {
+    name: "Sarath Meduru",
+    roles: [
+      "Full Stack Developer",
+      "Coder",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Sarath0506",
+    resume:
+      "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/meduru-sarath-671a19222/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "Tailwindcss",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-dbCeFK35gKsYwPyY5ETl6YkvakRLF38Qnw&s",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:
+            "https://www.manektech.com/storage/developer/1646733543.webp",
+        },
+        {
+          name: "SQL",
+          image:
+            "https://www.rogersoft.com/storage/attachments/1694676692.jpg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [        
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "Render",
+          image:
+            "https://media.licdn.com/dms/image/v2/D4E0BAQGGDoFoqHtOvA/company-logo_200_200/company-logo_200_200/0/1702595267620/renderco_logo?e=2147483647&v=beta&t=ZYrxKUyruOEupgw5Lr5amgwgBCJq8VXH8r05Qr5CeQc",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "C++",
+          image:
+            "https://banner2.cleanpng.com/20180629/eit/aaycsicwp.webp",
+        },
+      ],
+    },
+  ];
+  
+  // export const experiences = [
+  //   {
+  //     id: 0,
+  //     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+  //     role: "Frontend Engineer Intern",
+  //     company: "Neurobit",
+  //     date: "June 2023 - Nov 2023",
+  //     desc: "Enhanced user experiences on Neurobit PSG & Hybrid, Portals by resolving bugs & reduced load time by 40%. Built Neurobit Analytics portal using React Js with seamless interaction of REST APIs using AXIOS optimized with React Query. Refactored previous code to TypeScript, updated with new dependency and integrated Vite with Jest for Unit Testing.",
+  //     skills: [
+  //       "ReactJS",
+  //       "Redux",
+  //       "NodeJs",
+  //       "Material UI",
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //     ],
+  //     doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  //   },
+  // ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/en/1/1f/National_Institute_of_Technology%2C_Raipur_Logo.png",
+      school: "National Institute of Technology Raipur",
+      date: "Dec 2020 - Sep 2024",
+      grade: "7.75 CGPA",
+      desc: "I graduated with a Bachelor’s degree in Electronics and Communication Engineering from the National Institute of Technology (NIT) Raipur. My academic journey provided a solid foundation in both electronics and communication principles, along with a strong analytical skill set. This experience has equipped me to approach problem-solving with a technical perspective, which I apply in my work as a developer. I’m driven by a passion for technology and innovation and am eager to leverage my engineering background to contribute meaningfully to projects that challenge and excite me.",
+      degree: "Bachelor of Technology - BTech, Electronics and Communication Engineering",
+    },
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Narayana_Group_Logo.png/194px-Narayana_Group_Logo.png",
+      school: "Narayana Jr college, Narasimhakonda",
+      date: "Apr 2018 - Apr 2020",
+      grade: "90.06%",
+      desc: "I completed my class 12 high school education at Narayana Jr college",
+      degree: "SSC(XII), MPC",
+    },
+    {
+      id: 2,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Narayana_Group_Logo.png/194px-Narayana_Group_Logo.png",
+      school: "Narayana EM High School, DanalakshmiPuram",
+      date: "Apr 2018",
+      grade: "93.1%",
+      desc: "I completed my class 10 education at Narayana EM High School, where I studied Science.",
+      degree: "SSC(X), Science",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Course app (EduQuest)",
+      date: "Jul 2024 - Aug 2024",
+      description:
+        "An online learning platform like Bujus built with MERN stack containing user roles like Admin, Student, and Instructor, supporting course creation, enrollment, and progress tracking..",
+      image: eduquestImage,
+      tags: [
+        "JavaScript",
+        "React Js",
+        "MongoDB",
+        "Node Js",
+        "Express Js",
+        "Redux",
+        "Tailwindcss",
+      ],
+      category: "web app",
+      github: "https://github.com/Sarath0506/EdeQuest-frontend",
+      webapp: "https://edequest-frontend.onrender.com/",
+    },
+    {
+      id: 1,
+      title: "Shoping Cart",
+      date: "Mar 2024",
+      description:
+        "Created a responsive e-commerce application with product listings and includes actions and reducers for cart management using Redux.",
+      image: shoppingCartImage,
+      tags: [
+        "JavaScript",
+        "React Js",
+        "Redux",
+        "Tailwindcss"
+      ],
+      category: "web app",
+      github: "https://github.com/Sarath0506/shop-app",
+      webapp: "https://sarath0506.github.io/",
+    },
+    {
+      id: 2,
+      title: "Power prediction",
+      date: "Jun 2023",
+      description:
+        "Predicting power profile is essential for optimal utilisation of economy and resources.",
+      image: powerPredictionImage,
+      tags: ["Python", "Keras", "TensorFlow","JupyterNotebook"],
+      category: "machine learning",
+      github: "https://github.com/Sarath0506/Power-Predictor",
+      webapp: "#",
+    },
+  ];
